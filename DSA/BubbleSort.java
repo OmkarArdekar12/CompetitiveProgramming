8c017c930f5f8821377996464ee7db8daed5b43a
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-08-29 10:45:01 +0530

    Bubble Sort

--- DSA/BubbleSort.java
+++ DSA/BubbleSort.java
@@ -18,14 +18,16 @@ public class BubbleSort {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the Size of the Array : "));
+        System.out.print("Enter the Size of the Array: ");
         int n = sc.nextInt();
         int arr[] = new int[n];
+        System.out.println("Enter the Array Elements:");
         for(int i=0; i<n; ++i) {
             arr[i] = sc.nextInt();
         }
-        System.out.println("Original Array : " + Arrays.toString(arr));
+        System.out.println("Original Array: " + Arrays.toString(arr));
         bubbleSort(arr);
-        System.out.println("Sorted Array : " + Arrays.toString(arr));
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+        sc.close();
     }
 }
